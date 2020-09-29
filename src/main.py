304793afbb96e1ceb45eabad4bc6445217e2ddde
pthompson <thomppj@gmail.com>
2020-09-29 13:42:07 -0500

    updating model and main files

--- src/main.py
+++ src/main.py
@@ -1,24 +1,29 @@
-from model import UnregulatedGeneExpression
+from model import UnregulatedGeneExpression, GillespieUnregulatedGeneExpression
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import pandas
 import random
 
 
-# k0, k1, dm, dp
-const = [0.1, 0.1, 0.1, 0.1]
-model = UnregulatedGeneExpression(0, 0, const)
+n_A = 6.023E23  # Avogadro's Number
+
+# k0 (mRNA), k1 (protein), dm, dp
+const = [1, 1, .2, .01]
+odemodel = UnregulatedGeneExpression(0, 0, const=const)
+
+e_coli_vol = 6.5E-16  # Liters
 
 
-def main():
+def ode_sim():
     # initial condition
-    z0 = [0.1, 0.3]
+    z0 = [6e-6, 5e-7]
 
     # number of time points
     n = 10000
 
     # time points
-    t = np.linspace(0, 1000, n)
+    t = np.linspace(0, 10, n)
 
     # store solutions
     m = np.empty_like(t)
@@ -34,7 +39,7 @@ def main():
         tspan = [t[i - 1], t[i]]
 
         # solve for next step
-        z = odeint(model.unregulated_gene_expression, z0, tspan)
+        z = odeint(odemodel.unregulated_gene_expression, z0, tspan)
 
         # store solution for plotting
         m[i] = z[1][0]
@@ -42,17 +47,72 @@ def main():
 
         # next initial condition
         z0 = z[1]
-    plt.subplot(211)
-    plt.plot(t, m, 'r--', label='A(t)')
-    plt.grid(True)
-    plt.legend(loc='best')
+    return m, p, t
 
-    plt.subplot(212)
-    plt.plot(t, p, 'r--', label='R(t)')
-    plt.grid(True)
-    plt.legend(loc='best')
 
+def Gillespie_sim():
+    # k0 (mRNA), k1 (protein), dm, dp
+    initial_conditions = [1, 1, .2, .01]
+    gill_model = GillespieUnregulatedGeneExpression(tmax=10, m0=6e-6, p0=5e-7, const=initial_conditions)
+
+    data = []
+    t = 0  # start time
+    r0 = gill_model.initial_state()
+    data.append([r0, t])
+
+    while t <= 10:
+
+        a = gill_model.update_propensities(r0[0], r0[1])
+        next_rxn = gill_model.next_reaction(a)
+        t = gill_model.time_to_next_rxn(a, t)
+        r = gill_model.update_reaction_vector(r0, next_rxn)
+        data.append([r, t])
+        r0 = r
+
+    df = pandas.DataFrame(data)
+    return df
+
+
+
+def main():
+
+    """Plot ODE simulation"""
+    fig = plt.figure()
+    ax1 = fig.add_subplot()
+    ax1.plot(ode_sim()[2], ode_sim()[0], 'r--', label='m(t)')
+    ax1.plot(ode_sim()[2], ode_sim()[1], 'b--', label='p(t)')
+
+    ax1.grid(True)
+    ax1.legend(loc='best')
+    ax1.set_title('Concentrations of mRNA and protein over time (ODE)')
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Concentration')
+
+
+    """Extract Gillespie data"""
+    gill_data = Gillespie_sim()
+    proteins = []
+    mrna = []
+    molecular_data = gill_data[0]
+    for i in molecular_data:
+        mrna.append(i[0])
+        proteins.append(i[1])
+    time = gill_data[1]
+
+    """Plot Gillespie sim"""
+    fig1 = plt.figure()
+    ax2 = fig1.add_subplot()
+    ax2.plot(time, mrna, 'r', label='m(t)')
+    ax2.plot(time, proteins, 'b', label='p(t)')
+    ax2.grid(True)
+    ax2.legend(loc='best')
+    ax2.set_title('Numhber of mRNA and protein over time (Gillespie)')
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Number of molecules')
     plt.show()
+
+
+
 
 if __name__ == '__main__':
     main()
--- src/model.py
+++ src/model.py
@@ -1,14 +1,13 @@
-
 import math
-
-n_A = 6.023E23 #Avogadro's Number
-
+import random
+import numpy
 """
 This models the ODE of an unregulated genetic expression of a prokaryotic cell
 k0 is the rate at which mRNA is being generated
 k1 is the rate at which proteins are being 
 dm and dp are the degradation constants of mRNA and proteins respectively
 C1 and C2 are integration constants
+m0 and p0 are the mRNA and protein concentrations in M
 """
 
 
@@ -16,29 +15,30 @@ class UnregulatedGeneExpression:
 
     # t0 is a single number and t is a vector of numbers
     # const is an array of numbers that represent the constants of the reactions
-    def __init__(self, t0, t, const):
+    def __init__(self, t0=None, t=None, m0=0, p0=0, const=(1, 1, .1, .1)):
         self.t0 = t0
         self.t = t
+        self.m0 = m0
+        self.p0 = p0
         self.k0 = const[0]
         self.k1 = const[1]
         self.dm = const[2]
         self.dp = const[3]
 
-    def unregulated_m_rna(self):
-        m_rna = C1 * math.exp(-self.dm * self.t0) + self.k0/self.dm
-        return m_rna
-
-    def unregulated_protein(self):
-        protein = (self.k1 * self.k0) / (self.dm * self.dp) + (C1 * math.exp(-self.dm * self.t0))/(self.dp - self.dm) \
+    def solved_unregulated(self):
+        C1 = self.m0 - self.k0 / self.dm
+        C2 = self.p0 - (self.k1 * self.k0) / (self.dm * self.dp) - C1 / (self.dp - self.dm)
+        m_rna = C1 * math.exp(-self.dm * self.t0) + self.k0 / self.dm
+        protein = (self.k1 * self.k0) / (self.dm * self.dp) + (C1 * math.exp(-self.dm * self.t0)) / (self.dp - self.dm) \
                   + C2 * math.exp(-self.dp * self.t0)
-        return protein
+        return m_rna, protein
 
     # Use scipy.odeint to evaluate
-    def unregulated_gene_expression(self, z, t=.1):
+    def unregulated_gene_expression(self, z, t):
         m0 = z[0]
         p0 = z[1]
         dmdt = self.k0 - self.dm * m0
-        dpdt = self.k1 * m0 - self.dp*p0
+        dpdt = self.k1 * m0 - self.dp * p0
         dzdt = dmdt, dpdt
         return dzdt
 
@@ -48,7 +48,25 @@ class UnregulatedGeneExpression:
         dpdt = (self.k1 * self.k0) / self.dm - self.dp * p0
         return 0, dpdt
 
-    def gillespie(self):
+
+class GillespieUnregulatedGeneExpression:
+
+    # const is an array of numbers that represent the constants of the reactions
+    def __init__(self, tmax=10, m0=0, p0=0, const=None):
+        if const is None:
+            const = [1, 1, .1, .1]
+        self.tmax = tmax
+        self.k0 = const[0]
+        self.k1 = const[1]
+        self.dm = const[2]
+        self.dp = const[3]
+        self.Nm = m0 * 6.5E-16 * 6.023E23
+        self.Np = p0 * 6.5E-16 * 6.023E23
+
+    def initial_state(self):
+        return [self.Nm, self.Np]
+
+    def update_propensities(self, Nm, Np):
         a1 = self.k0
         a2 = self.k1 * Nm
         a3 = self.dm * Nm
@@ -56,9 +74,37 @@ class UnregulatedGeneExpression:
 
         total = a1 + a2 + a3 + a4
 
+        r1 = a1/total
+        r2 = a2/total
+        r3 = a3/total
+        r4 = a4/total
 
+        return [r1, r2, r3, r4, total]
 
+    def next_reaction(self, r):
+        n = random.uniform(0, 1)
+        if 0 <= n <= r[0]:
+            return [1, 0]
+        elif r[0] < n <= r[1] + r[0]:
+            return [0, 1]
+        elif r[1] + r[0] < n <= r[2] + r[1] + r[0]:
+            return [-1, 0]
+        elif r[2] + r[1] + r[0] < n <= 1:
+            return [0, -1]
 
+    def time_to_next_rxn(self, a, t):
+        dt = -math.log(1 - random.uniform(0, 1))/a[4]
+        if t == self.tmax:
+            return
+        elif t != self.tmax:
+            t = t + dt
+            return t
 
+    def update_reaction_vector(self, current_state, update_vector):
+        # print(current_state, update_vector)
+        current_state = numpy.array(current_state)
+        update_vector = numpy.array(update_vector)
+        updated = current_state + update_vector
+        return updated
 
 
